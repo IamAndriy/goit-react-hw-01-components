987c803cd8e1ffa24949ff984328eebb4383dde9
Olga Dzubak <>
2025-01-20 07:01:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import reactDOM from "react-dom/client";
 import { App } from "components/App/App";
 import "./index.css"; 
 
+
 reactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
